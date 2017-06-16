c706814817ce7c0934b1e23e910f49bf6f14fed4
weihongwei <hwwei@thoughtworks.com>
2017-06-16 16:31:25 +0800

    init spring cloud netflix

---
+++ eureka-client/src/main/java/com/tw/springcloud/demo/netflix/eureka/client/Application.java
@@ -0,0 +1,27 @@
+package com.tw.springcloud.demo.netflix.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class Application {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ eureka-server/src/main/java/com/tw/springcloud/demo/netflix/eureka/server/Application.java
@@ -0,0 +1,16 @@
+package com.tw.springcloud.demo.netflix.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
