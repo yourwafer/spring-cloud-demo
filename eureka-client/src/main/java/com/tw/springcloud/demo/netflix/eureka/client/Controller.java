7d0ca99743c3e65431c522cf2345b5fe26d95399
weihongwei <hwwei@thoughtworks.com>
2017-06-19 14:08:50 +0800

    包含多个注册服务器

--- eureka-client/src/main/java/com/tw/springcloud/demo/netflix/eureka/client/Controller.java
+++ eureka-client/src/main/java/com/tw/springcloud/demo/netflix/eureka/client/Controller.java
@@ -1,24 +1,14 @@
 package com.tw.springcloud.demo.netflix.eureka.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableEurekaClient
 @RestController
-public class Application {
-
+public class Controller {
     @Autowired
     private DiscoveryClient client;
 
@@ -30,9 +20,4 @@ public class Application {
         });
         return "Hello World";
     }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
 }
--- eureka-server/src/main/java/com/tw/springcloud/demo/netflix/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/tw/springcloud/demo/netflix/eureka/server/EurekaServerApplication.java
@@ -2,14 +2,13 @@ package com.tw.springcloud.demo.netflix.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
