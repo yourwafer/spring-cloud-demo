30a1d24830cd28c1b027f453cea0ce9916985f15
weihongwei <hwwei@thoughtworks.com>
2017-06-22 11:43:58 +0800

    使用服务发现获取配置服的配置

--- config-client/src/main/java/com/thoughtworks/study/springcloudconfig/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/thoughtworks/study/springcloudconfig/client/ConfigClientApplication.java
@@ -3,11 +3,16 @@ package com.thoughtworks.study.springcloudconfig.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     @Value("client-name")
