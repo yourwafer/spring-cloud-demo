dcfeb29bf01f6c3ccb700f71d7a720eee550e1a4
weihongwei <hwwei@thoughtworks.com>
2017-06-22 15:15:06 +0800

    版本

--- cloud-bus/src/main/java/com/thoughtworks/study/springcloudconfig/client/CloudBusApplication.java
+++ cloud-bus/src/main/java/com/thoughtworks/study/springcloudconfig/client/CloudBusApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class CloudBusApplication {
 
     @Value("client-name")
